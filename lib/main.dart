556d00d14940a3a2623562b60594279e94f2d09c
karim2756 <karimmagdi1236@gmail.com>
2025-06-04 20:58:09 +0300

    added firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+
 import 'app/app.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   // await dotenv.load(fileName: ".env");
   // await di.init();
   await EasyLocalization.ensureInitialized();
   runApp(EasyLocalization(
-        supportedLocales: const [Locale("ar"),Locale("en"),],
-        fallbackLocale: const Locale('ar'),
-        saveLocale: true,
-        startLocale: const Locale('en'),
-        path: 'assets/translations',
-        child: const AquaSol(),
-      ));
+    supportedLocales: const [
+      Locale("ar"),
+      Locale("en"),
+    ],
+    fallbackLocale: const Locale('ar'),
+    saveLocale: true,
+    startLocale: const Locale('en'),
+    path: 'assets/translations',
+    child: const AquaSol(),
+  ));
 }
